886c65923ad55ab99c86562355c6beba620e0c1a
ranjanaak97 <45234412+ranjanaak97@users.noreply.github.com>
2020-08-01 21:00:27 +0530

    Update users.js

--- schemas/users.js
+++ schemas/users.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose;
 const UserSchema = new Schema({
-  name: String,
-  displayname: String,
+  //name: String,
+  //displayname: String,             //i have commented this since in login.js u are only sending email and password so only those should be there
   email: String,
   password: String
 })
